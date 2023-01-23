af714ec0264097131d1cdd4e55b0613b40dbb0cb
pedrodumbledore <108952479+pedrodumbledore@users.noreply.github.com>
2023-01-23 19:40:32 -0300

    Update jogodamemoria.js

--- jogodamemoria.js
+++ jogodamemoria.js
@@ -32,10 +32,10 @@ function Testa(){
     lin2=escolhidos[escolhidos.length-2][0];
     col2=escolhidos[escolhidos.length-2][1];
     if(tab[lin1][col1]==tab[lin2][col2]){
-        td=document.getElementById(""+lin1+col1);
-        td.removeAttribute("onclick");
-        td2=document.getElementById(""+lin2+col2);
-        td2.removeAttribute("onclick");
+        pos=posicoes.indexOf([lin1,col1]);
+        posicoes.splice(pos,1);
+        pos=posicoes.indexOf([lin2,col2]);
+        posicoes.splice(pos,1);
     }
     else{
         setTimeout(()=>{
@@ -43,6 +43,7 @@ function Testa(){
             img2=document.getElementById("1"+lin2+col2);
             img1.parentNode.removeChild(img1);
             img2.parentNode.removeChild(img2);
+            ColocaOnclick();
         },1000);
     }
     if(TestaTudo()==false){
@@ -69,5 +70,27 @@ function TestaTudo(){
     }
     return tof
 }
-var tab=[], escolhidos=[], jogador=0;
+function TiraOnclick(){
+    for(c=0;c<4;c++){
+        for(i=0;i<4;i++){
+            td=document.getElementById(""+c+i);
+            td.removeAttribute("onclick");
+        }
+    }
+}
+function ColocaOnclick(){
+    for(c=0;c<posicoes.length;c++){
+        lin=posicoes[c][0];
+        col=posicoes[c][1];
+        td=document.getElementById(""+lin+col);
+        on="escolhidos.push(["+lin+","+col+"]);Imagem("+lin+","+col+");if(escolhidos.length%2==0){Testa();TiraOnclick();}"
+        td.setAttribute("onclick",on);
+    }
+}
+var tab=[], escolhidos=[], posicoes=[];
+for(c=0;c<4;c++){
+    for(i=0;i<4;i++){
+        posicoes.push([c,i]);
+    }
+}
 SorteiaTab();
